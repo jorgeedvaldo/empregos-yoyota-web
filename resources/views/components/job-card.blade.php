6d61d39b8148ce0d6fa053058f6397ca183cfc34
jorgeedvaldo <edvaldojorge1996@gmail.com>
2024-03-14 23:49:33 +0000

    create views

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,105 @@
+<div class="relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-gray-50 p-6 sm:py-12">
+    <div class="job-card flex w-full my-4 max-w-4xl flex-col justify-between gap-3 rounded-md bg-white px-5 py-4 shadow-xl shadow-gray-100 sm:flex-row sm:items-center hover:shadow-lg hover:shadow-gray-400 transition duration-300 ease-in-out">
+      <div>
+        <span class="text-sm text-gray-700">Kwame Oil and Gas</span>
+        <h3 class="mt-px font-bold">Engenheiro Backend Full Stack Sênior</h3>
+        <p class="text-slate-600">O QHSE Group Transocean está focado em ser o empregador preferido para a indústria de perfuração. Nos desafiamos todos os dias para impulsionar o desempenho da empresa...</p>
+        <div class="mt-2 flex items-center gap-3">
+          <span class="rounded-full bg-gray-200 px-3 py-1 text-sm text-gray-700">14-04-2024</span>
+          <span class="flex items-center gap-1 text-sm text-slate-600">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+            Luanda
+          </span>
+        </div>
+      </div>
+      <div>
+        <button class="flex items-center gap-1 rounded-md bg-gray-900 px-4 py-2 font-medium text-white hover:bg-gray-800 transition duration-300 ease-in-out">
+          Detalhes
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+          </svg>
+        </button>
+      </div>
+    </div>
+
+    <div class="job-card flex w-full my-4 max-w-4xl flex-col justify-between gap-3 rounded-md bg-white px-5 py-4 shadow-xl shadow-gray-100 sm:flex-row sm:items-center hover:shadow-lg hover:shadow-gray-400 transition duration-300 ease-in-out">
+      <div>
+        <span class="text-sm text-gray-700">Kwame Oil and Gas</span>
+        <h3 class="mt-px font-bold">Engenheiro Backend Full Stack Sênior</h3>
+        <p class="text-slate-600">O QHSE Group Transocean está focado em ser o empregador preferido para a indústria de perfuração. Nos desafiamos todos os dias para impulsionar o desempenho da empresa...</p>
+        <div class="mt-2 flex items-center gap-3">
+          <span class="rounded-full bg-gray-200 px-3 py-1 text-sm text-gray-700">14-04-2024</span>
+          <span class="flex items-center gap-1 text-sm text-slate-600">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+            Luanda
+          </span>
+        </div>
+      </div>
+      <div>
+        <button class="flex items-center gap-1 rounded-md bg-gray-900 px-4 py-2 font-medium text-white hover:bg-gray-800 transition duration-300 ease-in-out">
+          Detalhes
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+          </svg>
+        </button>
+      </div>
+    </div>
+
+    <div class="job-card flex w-full my-4 max-w-4xl flex-col justify-between gap-3 rounded-md bg-white px-5 py-4 shadow-xl shadow-gray-100 sm:flex-row sm:items-center hover:shadow-lg hover:shadow-gray-400 transition duration-300 ease-in-out">
+      <div>
+        <span class="text-sm text-gray-700">Kwame Oil and Gas</span>
+        <h3 class="mt-px font-bold">Engenheiro Backend Full Stack Sênior</h3>
+        <p class="text-slate-600">O QHSE Group Transocean está focado em ser o empregador preferido para a indústria de perfuração. Nos desafiamos todos os dias para impulsionar o desempenho da empresa...</p>
+        <div class="mt-2 flex items-center gap-3">
+          <span class="rounded-full bg-gray-200 px-3 py-1 text-sm text-gray-700">14-04-2024</span>
+          <span class="flex items-center gap-1 text-sm text-slate-600">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+            Luanda
+          </span>
+        </div>
+      </div>
+      <div>
+        <button class="flex items-center gap-1 rounded-md bg-gray-900 px-4 py-2 font-medium text-white hover:bg-gray-800 transition duration-300 ease-in-out">
+          Detalhes
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+          </svg>
+        </button>
+      </div>
+    </div>
+
+    <div class="job-card flex w-full my-4 max-w-4xl flex-col justify-between gap-3 rounded-md bg-white px-5 py-4 shadow-xl shadow-gray-100 sm:flex-row sm:items-center hover:shadow-lg hover:shadow-gray-400 transition duration-300 ease-in-out">
+      <div>
+        <span class="text-sm text-gray-700">Kwame Oil and Gas</span>
+        <h3 class="mt-px font-bold">Engenheiro Backend Full Stack Sênior</h3>
+        <p class="text-slate-600">O QHSE Group Transocean está focado em ser o empregador preferido para a indústria de perfuração. Nos desafiamos todos os dias para impulsionar o desempenho da empresa...</p>
+        <div class="mt-2 flex items-center gap-3">
+          <span class="rounded-full bg-gray-200 px-3 py-1 text-sm text-gray-700">14-04-2024</span>
+          <span class="flex items-center gap-1 text-sm text-slate-600">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+            Luanda
+          </span>
+        </div>
+      </div>
+      <div>
+        <button class="flex items-center gap-1 rounded-md bg-gray-900 px-4 py-2 font-medium text-white hover:bg-gray-800 transition duration-300 ease-in-out">
+          Detalhes
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+          </svg>
+        </button>
+      </div>
+    </div>
+  </div>
